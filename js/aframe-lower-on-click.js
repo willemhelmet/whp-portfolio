8b8422884df0b12de6912c376d0f07b923aadd06
Willem Pickleman <willem.pickleman@digitas.com>
2019-03-06 16:25:35 -0600

    Initial Upload

---
+++ js/aframe-lower-on-click.js
@@ -0,0 +1,19 @@
+AFRAME.registerComponent("lower-on-click", {
+
+    schema:
+    {
+        index: {type: 'string'}
+    },
+
+    init: function()
+    {
+        // I don't know why making the data in the schema makes everything work -- but it does!
+        var index_str = this.data.index;
+
+        this.el.addEventListener('click', function (event)
+        {
+            //console.log('time-to-lower-' + index_str)
+            this.emit('time-to-lower-' + index_str);
+        });
+    }
+});
