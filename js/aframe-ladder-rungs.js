331f734ae46082d3439c1bf70c5533df8dd753a5
Willem Pickleman <willem.pickleman@digitas.com>
2019-03-07 15:51:50 -0600

    What happens if I do this?

---
+++ js/aframe-ladder-rungs.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent("ladder-rungs", {
+
+    schema:
+    {
+        frequency: {type: 'number'},
+        amount: {type: 'number'}
+    },
+
+    init: function()
+    {
+        // I don't know why making the data in the schema a variable makes everything work -- but it does!
+        var frequency = this.data.frequency;
+        var amount = this.data.amount;
+        
+        console.log(this.el);
+
+        for (var i = 0; i < amount; i++)
+        {
+            var rung = document.createElement('a-box')
+            this.el.appendChild(rung);
+
+            rung.setAttribute('scale', {x: .6, y: .07, z: .1});
+            rung.setAttribute('material', 'shader', 'flat');
+            rung.setAttribute('position', {x: 0, y: frequency * i, z: 0})
+        }
+
+    }
+});
